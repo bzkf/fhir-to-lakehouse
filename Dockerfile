5d23b8905860585b40053ad27bff90777d7d94f1
chgl <chgl@users.noreply.github.com>
2025-02-04 21:32:47 +0100

    test: added integration tests to ci

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,14 @@ FROM docker.io/library/spark:3.5.4-scala2.12-java17-python3-ubuntu@sha256:f53684
 ENV PYTHONDONTWRITEBYTECODE=1 \
     PYTHONUNBUFFERED=1 \
     METRICS_ADDR=0.0.0.0
-WORKDIR /home/spark
 USER 185:185
+WORKDIR /home/spark
 
-RUN --mount=type=cache,target=/home/spark/.cache/pip \
-    --mount=type=bind,source=requirements.txt,target=requirements.txt \
-    python3 -m pip install --no-cache-dir -r requirements.txt
+COPY requirements.txt .
 
-COPY --chown=185:185 . .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
 
 RUN SPARK_INSTALL_PACKAGES_AND_EXIT=1 python3 src/main.py
 
--- tests/integration/test_deployment_in_k8s.py
+++ tests/integration/test_deployment_in_k8s.py
@@ -1,80 +1,49 @@
 import os
 from pathlib import Path
+from confluent_kafka import Producer
+from loguru import logger
+from deltalake import DeltaTable
 
-import pytest
-import yaml
-from kubernetes import client, config
-from pyhelm3 import Client
-from testcontainers.k3s import K3SContainer
 
 HERE = Path(os.path.abspath(os.path.dirname(__file__)))
 
-k3s = K3SContainer("docker.io/rancher/k3s:v1.32.1-k3s1")
 
-
-@pytest.fixture(scope="module")
-def setup_k3s(request):
-    k3s.start()
-
-    def remove_container():
-        k3s.stop()
-
-    request.addfinalizer(remove_container)
-
-
-@pytest.mark.asyncio
-async def test_deploy_to_k8s_should_create_delta_tables(setup_k3s, tmp_path):
-    config.load_kube_config_from_dict(yaml.safe_load(k3s.config_yaml()))
-    kubeconfig_path = tmp_path / "kubeconfig.yaml"
-    kubeconfig_path.write_text(k3s.config_yaml())
-
-    helm_client = Client(kubeconfig=kubeconfig_path)
-
-    chart = await helm_client.get_chart(HERE / "fixtures")
-
-    # XXX: currently requires the chart dependencies to be installed
-    #      before calling it: `helm dep up tests/integration/fixtures`
-    revision = await helm_client.install_or_upgrade_release(
-        release_name="fixtures",
-        chart=chart,
-        wait=True,
-    )
-
-    print(
-        revision.release.name,
-        revision.release.namespace,
-        revision.revision,
-        str(revision.status),
-    )
-
-    pod = client.CoreV1Api().list_pod_for_all_namespaces(limit=1)
-
-    (x, y) = k3s.get_logs()
-
-    print(x.decode("utf-8"))
-    print(y.decode("utf-8"))
-
-    revision = await helm_client.install_or_upgrade_release(
-        release_name="fixtures",
-        chart=chart,
-        values={
-            "stream-processors.enabled": "true",
-            "stream-processors.processors.fhir-to-delta.image.tag": "test",
-        },
-        wait=True,
-    )
-
-    print(
-        revision.release.name,
-        revision.release.namespace,
-        revision.revision,
-        str(revision.status),
-    )
-
-    assert len(pod.items) > 0, "Unable to get running nodes from k3s cluster"
-    # install strimzi operator
-    # install kafka with tls
-    # install minio
-    # install this app using the stream-processor helm chart
+def test_deploy_to_k8s_should_create_delta_tables():
     # load bundles using this test function
     # check that the delta tables are created (after some time)
+    p = Producer({"bootstrap.servers": "localhost:30092"})
+
+    def delivery_report(err, msg):
+        if err is not None:
+            logger.info("Message delivery failed: {}".format(err))
+        else:
+            logger.info(
+                "Message delivered to {} [{}]".format(msg.topic(), msg.partition())
+            )
+
+    bundle = (
+        Path(HERE) / ".." / "unit" / "fixtures" / "resources" / "single-patient.json"
+    ).read_bytes()
+
+    p.poll(0)
+
+    p.produce("fhir.msg", bundle, callback=delivery_report)
+
+    p.flush()
+
+    patient_table_path = "s3://fhir/warehouse/Patient.parquet"
+    storage_options = {
+        "AWS_ACCESS_KEY_ID": "admin",
+        "AWS_SECRET_ACCESS_KEY": "miniopass",
+        "AWS_ENDPOINT_URL": "http://localhost:30900",
+        "AWS_VIRTUAL_HOSTED_STYLE_REQUEST": "false",
+    }
+
+    assert DeltaTable.is_deltatable(
+        patient_table_path,
+        storage_options=storage_options,
+    ), "Delta table not created"
+
+    dt = DeltaTable(patient_table_path, storage_options=storage_options)
+
+    assert dt.to_pandas().count() == 1, "Delta table not populated"
