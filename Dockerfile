decc315d247b89e5ae9c561fd32bde1a3330e71a
chgl <chgl@users.noreply.github.com>
2025-02-05 22:55:16 +0100

    fix: added integration tests to ci and fixed image build (#23)
    * test: added integration tests to ci
    
    * chore: update CI workflow to include code checkout and change Dockerfile user to root
    
    * refactor: reorganize imports in integration test for Kubernetes deployment
    
    * chore: update CI configuration and Dockerfile for improved setup and user permissions
    
    * logs
    
    * chore: optimize Dockerfile by simplifying installation process and improving layer caching
    
    * chore: update CI configuration and Dockerfile for dynamic image tag and improved caching
    
    * chore: enhance Dockerfile and values.yaml for improved security and caching
    
    * chore: update CI configuration and dependencies for Kafka integration and testing

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,26 @@
-FROM docker.io/library/spark:3.5.4-scala2.12-java17-python3-ubuntu@sha256:f536843be0fef2545bb8db1ce210ccfd6966eff87c9f519922f21cca1cfc4a8b AS base
+FROM docker.io/apache/spark:3.5.4-scala2.12-java17-python3-ubuntu
 ENV PYTHONDONTWRITEBYTECODE=1 \
     PYTHONUNBUFFERED=1 \
     METRICS_ADDR=0.0.0.0
-WORKDIR /home/spark
-USER 185:185
+WORKDIR /opt/fhir-to-delta
+USER root
+
+RUN <<EOF
+set -e
+mkdir /home/spark
+chown -R spark:spark /home/spark
+EOF
+
+COPY requirements.txt .
 
 RUN --mount=type=cache,target=/home/spark/.cache/pip \
     --mount=type=bind,source=requirements.txt,target=requirements.txt \
     python3 -m pip install --no-cache-dir -r requirements.txt
 
-COPY --chown=185:185 . .
+COPY src/ src/
 
+USER 185:185
 RUN SPARK_INSTALL_PACKAGES_AND_EXIT=1 python3 src/main.py
 
-ENTRYPOINT [ "python3", "src/main.py" ]
+WORKDIR /home/spark
+ENTRYPOINT [ "python3", "/opt/fhir-to-delta/src/main.py" ]
--- tests/integration/test_deployment_in_k8s.py
+++ tests/integration/test_deployment_in_k8s.py
@@ -1,80 +1,31 @@
 import os
 from pathlib import Path
 
-import pytest
-import yaml
-from kubernetes import client, config
-from pyhelm3 import Client
-from testcontainers.k3s import K3SContainer
+from kafka import KafkaProducer
+from deltalake import DeltaTable
 
 HERE = Path(os.path.abspath(os.path.dirname(__file__)))
 
-k3s = K3SContainer("docker.io/rancher/k3s:v1.32.1-k3s1")
 
+def test_deploy_to_k8s_should_create_delta_tables():
+    producer = KafkaProducer(bootstrap_servers="localhost:30092")
 
-@pytest.fixture(scope="module")
-def setup_k3s(request):
-    k3s.start()
+    bundle = (
+        Path(HERE) / ".." / "unit" / "fixtures" / "resources" / "single-patient.json"
+    ).read_bytes()
 
-    def remove_container():
-        k3s.stop()
+    producer.send("fhir.msg", bundle)
 
-    request.addfinalizer(remove_container)
+    producer.flush()
 
+    patient_table_path = "s3://fhir/warehouse/Patient.parquet"
+    storage_options = {
+        "AWS_ACCESS_KEY_ID": "admin",
+        "AWS_SECRET_ACCESS_KEY": "miniopass",
+        "AWS_ENDPOINT_URL": "http://localhost:30900",
+        "AWS_VIRTUAL_HOSTED_STYLE_REQUEST": "false",
+    }
 
-@pytest.mark.asyncio
-async def test_deploy_to_k8s_should_create_delta_tables(setup_k3s, tmp_path):
-    config.load_kube_config_from_dict(yaml.safe_load(k3s.config_yaml()))
-    kubeconfig_path = tmp_path / "kubeconfig.yaml"
-    kubeconfig_path.write_text(k3s.config_yaml())
+    dt = DeltaTable(patient_table_path, storage_options=storage_options)
 
-    helm_client = Client(kubeconfig=kubeconfig_path)
-
-    chart = await helm_client.get_chart(HERE / "fixtures")
-
-    # XXX: currently requires the chart dependencies to be installed
-    #      before calling it: `helm dep up tests/integration/fixtures`
-    revision = await helm_client.install_or_upgrade_release(
-        release_name="fixtures",
-        chart=chart,
-        wait=True,
-    )
-
-    print(
-        revision.release.name,
-        revision.release.namespace,
-        revision.revision,
-        str(revision.status),
-    )
-
-    pod = client.CoreV1Api().list_pod_for_all_namespaces(limit=1)
-
-    (x, y) = k3s.get_logs()
-
-    print(x.decode("utf-8"))
-    print(y.decode("utf-8"))
-
-    revision = await helm_client.install_or_upgrade_release(
-        release_name="fixtures",
-        chart=chart,
-        values={
-            "stream-processors.enabled": "true",
-            "stream-processors.processors.fhir-to-delta.image.tag": "test",
-        },
-        wait=True,
-    )
-
-    print(
-        revision.release.name,
-        revision.release.namespace,
-        revision.revision,
-        str(revision.status),
-    )
-
-    assert len(pod.items) > 0, "Unable to get running nodes from k3s cluster"
-    # install strimzi operator
-    # install kafka with tls
-    # install minio
-    # install this app using the stream-processor helm chart
-    # load bundles using this test function
-    # check that the delta tables are created (after some time)
+    assert dt.to_pandas().count() == 1, "Delta table not populated"
