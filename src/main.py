ef6f63fe09be40d6e45c6665583289b29b3596cb
chgl <5307555+chgl@users.noreply.github.com>
2025-12-07 19:35:32 +0100

    bump version

--- src/main.py
+++ src/main.py
@@ -46,7 +46,7 @@ spark_config = (
         "spark.jars.packages",
         ",".join(
             [
-                "au.csiro.pathling:library-runtime:9.0.0",
+                "au.csiro.pathling:library-runtime:9.1.0",
                 "io.delta:delta-spark_2.13:4.0.0",
                 "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.1",
                 "org.apache.hadoop:hadoop-aws:3.4.1",
