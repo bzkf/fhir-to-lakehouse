6b554e21aaa0a36f9c0e939c44dcdcd6dba96cd5
chgl <chgl@users.noreply.github.com>
2026-02-15 16:33:53 +0100

    chore(deps): pathling 9.3.0 and go back to default, non-kryo serializer

--- src/main.py
+++ src/main.py
@@ -46,7 +46,7 @@ spark_config = (
         "spark.jars.packages",
         ",".join(
             [
-                "au.csiro.pathling:library-runtime:9.1.0",
+                "au.csiro.pathling:library-runtime:9.3.0",
                 "io.delta:delta-spark_2.13:4.0.0",
                 "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.1",
                 "org.apache.hadoop:hadoop-aws:3.4.1",
@@ -75,7 +75,6 @@ spark_config = (
         "spark.sql.catalog.spark_catalog",
         "org.apache.spark.sql.delta.catalog.DeltaCatalog",
     )
-    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     .config(
         "spark.hadoop.fs.s3a.path.style.access",
         "true",
